a8b6698840378ff8984eef0231c772ace1f1def6
Megan Flowers <brittanymflowers@gmail.com>
2021-06-17 11:28:54 -0600

    es lint disable

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -1,17 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function Planet({ selected }) {
     const [planet, setPlanet] = useState('')
 
-    const thisSpecies = () => {
+    const thisPlanet = () => {
         axios(selected.homeworld).then(({ data }) => {
             let stuff = data.name;
             setPlanet(stuff)
         })
     };
     useEffect(() => {
-        thisSpecies();
+        thisPlanet();
     }, [])
 
 return (
